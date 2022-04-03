d3bd394c1f9a8e558b60b61b4bdb36f14e539f8e
Kuba <Kuba@KUBA-KOMPUTER>
2022-04-03 21:08:35 +0200

    Dodaj pliki projektów.

---
+++ j.pal zad 3/j.pal zad 3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "klasa.h"
+using namespace std;
+int main()
+{
+    A a;
+    B b;
+    C c;
+    int x;
+    //    b.metoda();
+    c.metoda();
+    // a
+//    x = a.wart1;
+//    x = a.wart2;
+//    x = a.wart3;
+//    // b
+//    x = b.wart1;
+//    x = b.wart2;
+//    x = b.wart3;
+//    x = b.wart4;
+//    x = b.wart5;
+//    x = b.wart6;
+    // c
+
+    x = c.wart1;
+    x = c.wart2;
+    x = c.wart3;
+    //////////////////////////////
+    // private z b
+//    x = c.wart4;
+//    // protected z b
+//    x = c.wart5;
+//    // public z b
+//    x = c.wart6;
+//    ////////////////////////////
+//    // private
+//    x = c.wart7;
+//    // protected
+//    x = c.wart8;
+//    // public
+//    x = c.wart9;
+}
